e37c8b7cf43310b03720e8169441c6f88a7bbc52
noor848 <54281955+noor848@users.noreply.github.com>
2022-07-31 16:46:38 +0300

    Code works successfully

---
+++ Task5.cs
@@ -0,0 +1,30 @@
+﻿namespace Task5;
+public class Task5 { 
+    public static bool IsSubsequence(string s, string t)
+    {   int k = 0;
+        int j = 0;
+        int flag = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            while (j < t.Length)
+            { if (s[i] == t[j])
+                { 
+                    flag = 1;
+                    j++;
+                    break;
+                }
+                j++;
+            }
+            if (flag == 0)
+                return false;
+            flag = 0;
+        }
+        return true;
+    }
+    public static void Main(string[] args)
+    {
+       string s = "abc",t = "ahbgdc";
+        Console.WriteLine(IsSubsequence(s,t));
+    }
+
+}
