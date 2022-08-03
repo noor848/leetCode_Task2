dcca6a32164c45068a75523967bd8db2129d9d60
noor848 <54281955+noor848@users.noreply.github.com>
2022-08-04 00:03:07 +0300

    Separate Main in different class

--- Task1.cs
+++ Task1.cs
@@ -10,7 +10,7 @@
 
 public class Task1
 {  
-public static ListNode DetectCycle(ListNode head)
+ public static ListNode DetectCycle(ListNode head)
     {   ListNode listOne = head;
         ListNode listTwo = head;
         Dictionary<ListNode, ListNode> NodeStore = new Dictionary<ListNode, ListNode>();
@@ -29,40 +29,5 @@ public static ListNode DetectCycle(ListNode head)
         return null;
     }
 
-    public static void Main(string[] args)
-    {
-        int[] arr = { 1,2,3};
-        if (arr.Length == 1)
-            Console.WriteLine("no Cycle");
-        else
-        {
-            ListNode listNode = new ListNode(arr[0]);
-            ListNode head = listNode;
-            int i = 1;
-            while (i < arr.Length)
-            {
-                ListNode listNode2 = new ListNode(arr[i]);
-                head.next = listNode2;
-                head = head.next;
-                i++;
-            }
 
-            int position = 1;///position of Tail Connection 
-            ListNode head3 = listNode;
-            while (position != 0)
-            {
-                head3 = head3.next;
-                position--;
-            }
-            head.next = head3;
-            Task1.DetectCycle(listNode);
-            int index = 0;
-            while (head3 != listNode)
-            {
-                listNode = listNode.next;
-                index++;
-            }
-            Console.WriteLine("Position is At :"+index);
-        }
     }
-}
