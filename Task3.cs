e67ff539129c51c5f1a19d3aa6c405ab8f1dac17
noor848 <54281955+noor848@users.noreply.github.com>
2022-08-07 00:27:45 +0300

    Separate Main method in different class

--- Task3.cs
+++ Task3.cs
@@ -26,28 +26,6 @@ namespace Task3 {
 
             return k;
         }
-        public static void Main(string[] args)
-        {
-            int[] arr = { 1, 2, 3, 4, 5, 6 };
-            ListNode listNode = new ListNode(arr[0]);
-            ListNode head = listNode;
-
-            int i = 1;
-            while (i < arr.Length)
-            {
-                ListNode listNode2 = new ListNode(arr[i]);
-                head.next = listNode2;
-                head = head.next;
-                i++;
-            }
-            ListNode head3 = Task3.ReverseList(listNode);
-            while (head3 != null)
-            {
-                Console.Write(head3.val + " ");
-                head3 = head3.next;
-            }
-
-        }
     }
 
 }
