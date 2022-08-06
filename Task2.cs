a5ece1ee2cec22b9195f0861eb281033b209b723
noor848 <54281955+noor848@users.noreply.github.com>
2022-08-07 00:42:36 +0300

    Keep Main Method in class alone

--- Task2.cs
+++ Task2.cs
@@ -6,7 +6,7 @@
          this.next = next;
      }
  }
-public class Task2
+public class Task2 : Task2Main
 { public static ListNode2 MiddleNode(ListNode2 head)
     {
         ListNode2 listOne = head;
@@ -38,36 +38,5 @@ public class Task2
             }
         }
         return head.next;
-    }
-
-    public static void Main(string[] args)
-    {
-        int[] arr = { 1, 2, 3, 4, 5, 6 };
-        if (arr.Length == 1)
-        {
-            Console.WriteLine(arr[arr.Length - 1]);
-        }
-        else
-        {
-            ListNode2 listNode = new ListNode2(arr[0]);
-            ListNode2 head = listNode;
-
-            int i = 1;
-            while (i < arr.Length)
-            {
-                ListNode2 listNode2 = new ListNode2(arr[i]);
-                head.next = listNode2;
-                head = head.next;
-                i++;
-            }
-
-            ListNode2 head3 = Task2.MiddleNode(listNode);
-            while (head3 != null)
-            {
-                Console.Write(head3.val + " ");
-                head3 = head3.next;
-            }
-
-        }
     }
 }
